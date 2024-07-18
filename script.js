7cbddc75f2f10e71fb3b4ace0ee85ef488fe5aef
~D.t <165255878+Dave-07@users.noreply.github.com>
2024-07-18 14:35:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- const bodyEL = document.querySelector('body');        bodyEL.addEventListener('mousemove', (e) => {            const xPos = e.offsetX;            const yPos = e.offsetY;            const spanEl = document.createElement('span');											            const size = Math.floor(Math.random()*100);            bodyEL.appendChild(spanEl);            									            spanEl.style.width = `${size}px`;            spanEl.style.height = `${size}px`;												            spanEl.style.left = `${xPos}px`;            spanEl.style.top = `${yPos}px`;           		   		   		               setTimeout(()=> spanEl.remove(), 6000);        });
+ const bodyEL = document.querySelector('body');
+
+        bodyEL.addEventListener('mousemove', (e) => {
+            const xPos = e.offsetX;
+            const yPos = e.offsetY;
+            const spanEl = document.createElement('span');
+
+            const size = Math.floor(Math.random()*100);
+            bodyEL.appendChild(spanEl);
+            
+            spanEl.style.width = `${size}px`;
+            spanEl.style.height = `${size}px`;
+			
+            spanEl.style.left = `${xPos}px`;
+            spanEl.style.top = `${yPos}px`;
+           
+            setTimeout(()=> spanEl.remove(), 6000);
+        });
